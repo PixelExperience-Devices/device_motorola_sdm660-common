900ed6c42168fd3bd69af0c99fa0e96a0fa9c3e0
Erfan Abdi <erfangplus@gmail.com>
2018-06-19 00:54:57 +0430

    sdm660-common: fix thermal initialization
    * ro.hardware is qcom
    
    Change-Id: I067740c132782388a7fb178959bf85574568d62e
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- thermal/thermal-helper.cpp
+++ thermal/thermal-helper.cpp
@@ -50,7 +50,7 @@ static unsigned int gVrThrottledBelowMin;
  */
 bool initThermal() {
     std::string hardware = android::base::GetProperty("ro.hardware", "");
-    if (hardware == "sdm660") {
+    if (hardware == "qcom") {
         LOG(ERROR) << "Initialization on Sdm660";
         gSkinSensorNum = kSkinSensorNum;
         gSkinSensorType = kSkinSensorType;
