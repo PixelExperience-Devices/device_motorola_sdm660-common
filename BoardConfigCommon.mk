95a52ac9991775fc2aa864de2279e8a6e93e7bc0
Bruno Martins <bgcngm@gmail.com>
2018-09-03 00:40:59 +0200

    sdm660-common: Adapt for local LineageHW java overlays
    Change-Id: Icb2b9810217f4b4d442ab83a16f455bd2ed82071

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -178,8 +178,7 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_PROVIDES_LIBLIGHT := true
 
 # Lineage hardware
-BOARD_HARDWARE_CLASS += \
-    $(PLATFORM_PATH)/lineagehw
+JAVA_SOURCE_OVERLAYS := org.lineageos.hardware|$(PLATFORM_PATH)/lineagehw|**/*.java
 
 # NFC
 BOARD_NFC_CHIPSET := pn553
