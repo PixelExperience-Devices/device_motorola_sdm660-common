9b5571f1ae644267b768790bc12710653ede7fed
Erfan Abdi <erfangplus@gmail.com>
2018-11-19 19:27:49 +0200

    sdm660-common: Wheee Beckham!
    Change-Id: I963787786a7bf1b9531eef9090bad2b9cc36e76f
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ Device configuration for Motorola SDM660 Devices
 
 Supported devices :
 
-Payton, Evert, Chef
+Payton, Evert, Chef, Beckham
--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -37,7 +37,7 @@ fi
 setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" true
 
 # Copyright headers and guards
-write_headers "payton evert chef"
+write_headers "payton evert chef beckham"
 
 write_makefiles "$MY_DIR"/proprietary-files.txt true
 
