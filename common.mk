9a9bed049ddf095dcba352dbf2f71810b9c92c86
Akianonymus <anonymus.aki@gmail.com>
2019-05-06 09:27:13 +0530

    sdm660-common: Remove android.hardware.audio.effect@2.0-service
    * cuz doesn't exist
    
    Change-Id: Ibc090fc867dca3af2fb7dd58a6a88956bac8e16d

--- common.mk
+++ common.mk
@@ -42,7 +42,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@4.0-impl \
     android.hardware.audio.service \
     android.hardware.audio.effect@4.0-impl \
-    android.hardware.audio.effect@2.0-service \
     audio.a2dp.default \
     audio.r_submix.default \
     audio.usb.default \
