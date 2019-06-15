58acf13741150f0cd2a1005ad1070ea651a385aa
Hasaber8 <rohanhasabe8@gmail.com>
2019-06-16 03:35:26 +0530

    sdm660-common: Build vendor.display.config@1.3

--- common.mk
+++ common.mk
@@ -95,6 +95,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
+    vendor.display.config@1.3 \
     copybit.sdm660 \
     gralloc.sdm660 \
     hwcomposer.sdm660 \
