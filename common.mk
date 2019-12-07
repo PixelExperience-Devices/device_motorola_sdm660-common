a8a4eeabd808914bc8d39a5d3bf4a76f5ca263fe
Joshua Blanchard <joshua.lee.bbg@gmail.com>
2019-12-06 19:08:15 -0500

    sdm660-common: update qti_whitelist from lake QPW30.61-18
     * Setup and use TARGET_COPY_OUT_SYSTEM
    
    Change-Id: I4ec6ed8c7095cff8feaf8d17cef44454d6396b28
    Signed-off-by: Joshua Blanchard <joshua.lee.bbg@gmail.com>

--- common.mk
+++ common.mk
@@ -279,7 +279,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml \
-    $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
+    $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.vendor.qti.va_aosp.support=1
