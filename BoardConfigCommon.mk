7ad3798c1ebdb480fe16059927613f2d9ca1e127
Erfan Abdi <erfangplus@gmail.com>
2020-01-29 13:19:16 +0330

    sdm660-common: Set verity to EIO mode
    Change-Id: I64f12657ce0d0b7a8e364c5e5b1da26177dac184

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -129,6 +129,7 @@ BOARD_KERNEL_CMDLINE += swiotlb=2048 androidboot.configfs=true
 BOARD_KERNEL_CMDLINE += sched_enable_hmp=1 sched_enable_power_aware=1
 BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a800000.dwc3
 BOARD_KERNEL_CMDLINE += loop.max_part=7
+BOARD_KERNEL_CMDLINE += androidboot.veritymode=eio
 #BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
