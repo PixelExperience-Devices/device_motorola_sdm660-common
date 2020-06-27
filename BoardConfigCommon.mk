1c0b1c2d9663eb620ec496ac6fc104a7a2d11027
Erfan Abdi <erfangplus@gmail.com>
2020-06-27 18:26:17 +0430

    sdm660-common: Kang libinit from xiaomi sdm660 common tree
    Change-Id: I14618daea9861c9e4677223c5a0d62889c13cab9

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -123,6 +123,10 @@ TARGET_FS_CONFIG_GEN += \
     $(PLATFORM_PATH)/config.fs \
     $(PLATFORM_PATH)/mot_aids.fs
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(PLATFORM_PATH):libinit_sdm660
+TARGET_RECOVERY_DEVICE_MODULES := libinit_sdm660
+
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 ehci-hcd.park=3
 BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1 service_locator.enable=1
