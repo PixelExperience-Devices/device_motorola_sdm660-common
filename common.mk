5737525c186cad7b3b66e905deae2e900a3f6c92
Erfan Abdi <erfangplus@gmail.com>
2020-06-27 16:09:25 +0430

    sdm660-common: Cleanup bluetooth blobs
    * And build vendor.qti.hardware.btconfigstore@1.0 from source
    
    Change-Id: I89533bac6af3f685c64e61f4f7c2ad9a52d26be6

--- common.mk
+++ common.mk
@@ -74,7 +74,8 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0 \
     audio.bluetooth.default \
     android.hardware.bluetooth.audio@2.0-impl \
-    libbt-vendor
+    libbt-vendor \
+    vendor.qti.hardware.btconfigstore@1.0.vendor
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
