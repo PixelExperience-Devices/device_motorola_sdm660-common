c7637ecd706050a6a59e8c912903f3e5f487a50d
Nolen Johnson <johnsonnolen@gmail.com>
2020-11-23 14:46:29 -0500

    sdm660-common: Cleanup dead targets
    build/make/core/main.mk:1136: warning:  device/motorola/nash/lineage_nash.mk includes non-existent modules in PRODUCT_PACKAGES
    Offending entries:
    android.hardware.audio.effect@2.0-service
    android.hardware.graphics.composer@2.1-impl
    android.hardware.soundtrigger@2.2-service
    android.hidl.manager@1.0-java
    audiod
    copybit.msm8998
    dhcpcd.conf
    ebtables
    ethertypes
    libQWiFiSoftApCfg
    libextmedia_jni
    libgenlock
    libicuuc.vendor
    libnl_2
    liboverlay
    nqnfcee_access.xml
    nqnfcse_access.xml
    textclassifier.bundle1
    wifilogd
    wpa_supplicant_wcn.conf
    build/make/core/main.mk:1136: error: Build failed.
    15:35:26 ckati failed with: exit status 1
    
    To do this yourself, put the following line into
    lineage_$TARGET.mk:
    
    `$(call enforce-product-packages-exist,)`
    
    Note that the above flag must be in the product
    makefile to work.
    
    Change-Id: I0e4d93f4c3be077d49befa1f547d11d623ca74d5

--- common.mk
+++ common.mk
@@ -40,11 +40,8 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
-    android.hardware.audio.effect@2.0-service \
     android.hardware.audio.service \
     android.hardware.soundtrigger@2.2-impl \
-    android.hardware.soundtrigger@2.2-service \
-    audiod \
     audio.a2dp.default \
     audio.r_submix.default \
     audio.usb.default \
@@ -111,18 +108,14 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
-    copybit.sdm660 \
     gralloc.sdm660 \
     hwcomposer.sdm660 \
     memtrack.sdm660 \
     libdisplayconfig \
-    libgenlock \
-    liboverlay \
     libqdMetaData.system \
     libvulkan \
     libtinyxml
@@ -171,7 +164,6 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.base@1.0_system \
     android.hidl.manager@1.0 \
-    android.hidl.manager@1.0-java \
     libhidltransport \
     libhidltransport.vendor \
     libhwbinder \
@@ -206,11 +198,6 @@ PRODUCT_PACKAGES += \
     IPACM_cfg.xml \
     libipanat \
     liboffloadhal
-
-# IPv6 tethering
-PRODUCT_PACKAGES += \
-    ebtables \
-    ethertypes
 
 # IRQ
 PRODUCT_COPY_FILES += \
@@ -301,7 +288,6 @@ PRODUCT_ODM_PROPERTIES += \
 # OMX
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
-    libextmedia_jni \
     libOmxAacEnc \
     libOmxAmrEnc \
     libOmxCore \
@@ -378,10 +364,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     net.tethering.noprovisioning=true
 
-# TextClassifier
-PRODUCT_PACKAGES += \
-    textclassifier.bundle1
-
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.1-service.sdm660
@@ -405,10 +387,8 @@ PRODUCT_PACKAGES += \
 
 # VNDK
 # Update this list with what each blob is actually for
-# libicuuc: vendor.qti.hardware.qteeconnector@1.0-impl
 # libstdc++: camera.sdm660
 PRODUCT_PACKAGES += \
-    libicuuc.vendor \
     libstdc++.vendor \
     libgui_vendor \
     vndk_package
@@ -422,21 +402,16 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
-    dhcpcd.conf \
     hostapd \
     hostapd_cli \
-    libnl_2 \
     libnl \
     libqsap_sdk \
-    libQWiFiSoftApCfg \
     libwifi-hal-qcom \
     libwpa_client \
     wificond \
-    wifilogd \
     WifiOverlay \
     wpa_supplicant \
     wpa_supplicant.conf \
-    wpa_supplicant_wcn.conf
 
 # Wi-Fi Display
 PRODUCT_PACKAGES += \
