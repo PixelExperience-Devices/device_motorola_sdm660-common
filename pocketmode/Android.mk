d2a6dc5bf7bf37c4756a3070ac7e32ee43913118
dianlujitao <dianlujitao@lineageos.org>
2020-11-24 15:23:26 +0800

    sdm660-common: Add pocketmode to doze whitelist
    Change-Id: I4b9f2267cfd6fe95d08ea0eb6f7415ba5d81c1b1

--- pocketmode/Android.mk
+++ pocketmode/Android.mk
@@ -9,7 +9,16 @@ LOCAL_PACKAGE_NAME := MotoPocketMode
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_PRIVILEGED_MODULE := true
+LOCAL_REQUIRED_MODULES := pocketmode_whitelist.xml
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 include $(BUILD_PACKAGE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := pocketmode_whitelist.xml
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_RELATIVE_PATH := sysconfig
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
