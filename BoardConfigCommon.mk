843b48a2ae0d90f6910ef5cb55c23fe9679366fc
Joshua Blanchard <joshua.lee.bbg@gmail.com>
2020-12-05 10:46:30 -0500

    sdm660-common: Update vendor SPL to 2020-10-01
    * lake:10/QPWS30.61-21-18-3/323d8:user/release-keys
    
    Change-Id: I30e7d83fe8844f62f69a5792b6895a6a74e90b07

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -177,7 +177,7 @@ BOARD_ROOT_EXTRA_SYMLINKS := \
     /mnt/vendor/persist:/persist
 
 # Vendor Security Patch Level
-VENDOR_SECURITY_PATCH := 2019-11-01
+VENDOR_SECURITY_PATCH := 2020-10-01
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
