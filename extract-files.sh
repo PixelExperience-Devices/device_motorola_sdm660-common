bacd553723d8ecb43feb1d080c02553a9f8c1d3d
Michael Bestas <mkbestas@lineageos.org>
2020-12-19 02:53:35 +0200

    sdm660-common: Switch to standalone extract utils
    * Align with templates while we are at it
    
    Change-Id: Ife50623a3d1b8733f5cc76995c982d8950d39ad4

--- extract-files.sh
+++ extract-files.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 #
-# Copyright (C) 2018-2020 The LineageOS Project
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
+# Copyright (C) 2018-2020 The PixelExperience Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
 set -e
@@ -21,9 +13,9 @@ set -e
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-AOSP_ROOT="${MY_DIR}"/../../..
+ANDROID_ROOT="${MY_DIR}/../../.."
 
-HELPER="${AOSP_ROOT}/vendor/aosp/build/tools/extract_utils.sh"
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
@@ -35,8 +27,8 @@ CLEAN_VENDOR=true
 
 ONLY_COMMON=
 ONLY_TARGET=
-SECTION=
 KANG=
+SECTION=
 
 while [ "${#}" -gt 0 ]; do
     case "${1}" in
@@ -69,45 +61,39 @@ fi
 
 function blob_fixup() {
     case "${1}" in
+        # Fix xml version
+        product/etc/permissions/vendor.qti.hardware.data.connection-V1.0-java.xml | product/etc/permissions/vendor.qti.hardware.data.connection-V1.1-java.xml)
+            sed -i 's|xml version="2.0"|xml version="1.0"|g' "${2}"
+            ;;
+        # Load wrapped shim
+        vendor/lib64/libmdmcutback.so)
+            sed -i "s|libqsap_sdk.so|libqsapshim.so|g" "${2}"
+            ;;
+        # Fix missing symbols
+        vendor/lib64/libril-qc-hal-qmi.so)
+            "${PATCHELF}" --add-needed "libcutils_shim.so" "${2}"
+            ;;
 
-    # memset shim
-    vendor/bin/charge_only_mode)
-        patchelf --add-needed libmemset_shim.so "${2}"
-        ;;
-
-    # Load wrapped shim
-    vendor/lib64/libmdmcutback.so)
-        sed -i "s|libqsap_sdk.so|libqsapshim.so|g" "${2}"
-        ;;
-
-    # Fix missing symbols
-    vendor/lib64/libril-qc-hal-qmi.so)
-        patchelf --add-needed "libcutils_shim.so" "${2}"
-        ;;
-
-    # Fix xml version
-    product/etc/permissions/vendor.qti.hardware.data.connection-V1.0-java.xml | product/etc/permissions/vendor.qti.hardware.data.connection-V1.1-java.xml)
-        sed -i 's|xml version="2.0"|xml version="1.0"|g' "${2}"
-        ;;
-
+        # memset shim
+        vendor/bin/charge_only_mode)
+             "${PATCHELF}" --add-needed libmemset_shim.so "${2}"
+            ;;
     esac
 }
 
 if [ -z "${ONLY_TARGET}" ]; then
     # Initialize the helper for common device
-    setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${AOSP_ROOT}" true "${CLEAN_VENDOR}"
+    setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true "${CLEAN_VENDOR}"
 
-    extract "${MY_DIR}/proprietary-files.txt" "${SRC}" \
-            "${KANG}" --section "${SECTION}"
+    extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
 fi
 
 if [ -z "${ONLY_COMMON}" ] && [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
     # Reinitialize the helper for device
     source "${MY_DIR}/../${DEVICE}/extract-files.sh"
-    setup_vendor "${DEVICE}" "${VENDOR}" "${AOSP_ROOT}" false "${CLEAN_VENDOR}"
+    setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
-    extract "${MY_DIR}/../${DEVICE}/proprietary-files.txt" "${SRC}" \
-            "${KANG}" --section "${SECTION}"
+    extract "${MY_DIR}/../${DEVICE}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
 fi
 
 "${MY_DIR}/setup-makefiles.sh"
