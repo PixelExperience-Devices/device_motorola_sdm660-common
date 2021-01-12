c11ce67eae94784bf53f7756230c5049aace68e3
Arne Coucheron <arco68@gmail.com>
2021-01-12 05:45:19 +0100

    sdm660-common: Silence clang warnings during kernel compile
    clang-11: warning: argument unused during compilation:
    '-fuse-ld=lld' [-Wunused-command-line-argument]
    
    Change-Id: I66a629f3573e3fad8aa085ea115475f02d60a7ea

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -108,6 +108,10 @@ BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_SOURCE := kernel/motorola/msm8998
 
+# Kernel additional flags
+TARGET_KERNEL_ADDITIONAL_FLAGS := \
+    HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
+
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
 
