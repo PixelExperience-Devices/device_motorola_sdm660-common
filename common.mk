7e5c4370b83a512ae9ae2ee3bac7f608569f6135
Jorge Lucas <jorgelucas@pixelexperience.org>
2021-02-17 23:48:59 +0000

    sdm660-common: Use MotoActions from hardware/motorola
    Signed-off-by: Jorge Lucas <jorgelucas@pixelexperience.org>

--- common.mk
+++ common.mk
@@ -209,10 +209,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.sdm660
 
-# LineageActions
-PRODUCT_PACKAGES += \
-    LineageActions
-
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
@@ -222,6 +218,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
+
+# MotoActions
+PRODUCT_PACKAGES += \
+    MotoActions
 
 # Network
 PRODUCT_PACKAGES += \
