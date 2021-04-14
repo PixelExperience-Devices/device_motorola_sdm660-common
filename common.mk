baa22a5cc2f16393af055cbd0f000ea5b49fced1
Chirayu Desai <chirayudesai1@gmail.com>
2021-04-15 02:09:34 +0530

    sdm660-common: rro_overlays: Move CarrierConfig to RRO
    Change-Id: Ia57acd244608848f70fdcc30d5694c919db60671

--- common.mk
+++ common.mk
@@ -350,6 +350,7 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Telephony
 PRODUCT_PACKAGES += \
+    CarrierConfigOverlay \
     qti-telephony-hidl-wrapper \
     qti_telephony_hidl_wrapper.xml \
     qti-telephony-utils \
