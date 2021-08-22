7ec7e8768b700f33c5a12e9cbf5ad35143731ef7
Mathieu Chartier <mathieuc@google.com>
2021-08-22 16:31:23 +0800

    sdm660-common: Speed profile services and wifi-service to reduce RAM and storage
    Change-Id: I1d907b24cb2760ddd9cc65ac4335730c8e61a0a3

--- common.mk
+++ common.mk
@@ -373,6 +373,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
 # Telephony
 PRODUCT_PACKAGES += \
     CarrierConfigOverlay \
