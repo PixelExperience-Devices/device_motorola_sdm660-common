f37cc20ea9b6e358e3fddad8e9681c7a053d9d70
Jeferson <jroliveira.oliveira301@gmail.com>
2021-09-10 13:55:59 +0200

    sdm660-common: Decommonize debug.sf.latch_unsignaled prop
    * To not harm all devices,decommonize this prop to set per device choice
    
    Change-Id: I26619beb7056840f266d8b595bed22180ef236e3

--- properties.mk
+++ properties.mk
@@ -116,7 +116,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.enable_hwc_vds=0 \
     debug.sf.enable_gl_backpressure=1 \
     debug.sf.hw=1 \
-    debug.sf.latch_unsignaled=1 \
     vendor.gralloc.enable_fb_ubwc=1 \
     debug.sf.disable_backpressure=1 \
     dev.pm.dyn_samplingrate=1 \
