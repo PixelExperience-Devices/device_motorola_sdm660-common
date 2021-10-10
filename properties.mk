1e3b007376b4bcfd9bcfcdb47c19ee17c89035d7
Marc Bourgoin <themard85@gmail.com>
2021-10-10 04:55:01 -0600

    sdm660-common: Define encryption properties for SD Card
    Our devices run on 4.4 kernel which lacks support for newer FBE standards,
    so define ICE for SD Card encryption so that adoptable storage will work for us.
    
    Change-Id: Id0ac7b1e6728a6307aa5d59689ede8920270203c

--- properties.mk
+++ properties.mk
@@ -222,6 +222,11 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ril.subscription.types=NV,RUIM \
     telephony.lteOnCdmaDevice=1
 
+# SD Card Encryption
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.crypto.volume.contents_mode=ice \
+    ro.crypto.volume.filenames_mode=aes-256-cts \
+
 # Skia
 # (b/183612348): Enable skia reduceOpsTaskSplitting
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
