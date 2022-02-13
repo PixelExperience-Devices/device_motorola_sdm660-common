4efef6415f38bbc240a15f2d08704e65386f8c43
Wei Wang <wvw@google.com>
2022-02-13 14:28:38 +0530

    sdm660-common: rootdir: Remove CPUBW min freq setting
    * CPU BW is set to 200Mhz in QC MTP, since on Pixel, we also adjust CPU BW
    in touch PowerHint, we can relax the setting to 100Mhz.
    
    Bug: 69271302
    Test: boot
    Change-Id: Iead3634d9c6fb0b3edc24ab0a19d62157a6a1793

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -2800,7 +2800,6 @@ case "$target" in
             do
                 echo "bw_hwmon" > $cpubw/governor
                 echo 50 > $cpubw/polling_interval
-                echo 762 > $cpubw/min_freq
                 echo "1525 3143 4173 5195 5859 7759 9887 10327" > $cpubw/bw_hwmon/mbps_zones
                 echo 4  > $cpubw/bw_hwmon/sample_ms
                 echo 85 > $cpubw/bw_hwmon/io_percent
