e94953e6fa83939c3a17b94b23f13fe10c7bd106
Pascoato <viniciuspascoato@gmail.com>
2022-08-13 22:04:19 +0000

    sdm660-common: Build android.hardware.power@1.2
    - Fixes: I hwservicemanager: getTransport: Cannot find entry android.hardware.power@1.2::IPower/default in either framework or device VINTF manifest.
    
    Signed-off-by: Pascoato <viniciuspascoato@gmail.com>

--- common.mk
+++ common.mk
@@ -315,6 +315,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
+    android.hardware.power@1.2.vendor \
     android.hardware.power-service-qti \
     vendor.qti.hardware.perf@2.0.vendor
 
