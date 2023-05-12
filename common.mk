1a4c7b7a0ecb1ff3aecbb7e4922222cec913bb64
Georg Veichtlbauer <georg@vware.at>
2023-05-12 14:54:25 +0200

    sdm660-common: Downrev radio libs
    Change-Id: I53392979e52ced9d909c9979ce79498a058ca271

--- common.mk
+++ common.mk
@@ -334,8 +334,8 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.6.vendor \
-    android.hardware.radio.config@1.3.vendor \
+    android.hardware.radio@1.5.vendor \
+    android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
     android.hardware.secure_element@1.2.vendor
 
