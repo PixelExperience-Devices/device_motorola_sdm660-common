028d4234f84cb8a663a53f2da8a40ed3c2c90df8
Georg Veichtlbauer <georg@vware.at>
2023-05-22 22:52:06 +0200

    sdm660-common: Import perf configs from vendor
    Change-Id: If3ac427d50bd0a9cb8d2e2dbbcf23e4c45b27643

--- common.mk
+++ common.mk
@@ -320,7 +320,15 @@ PRODUCT_PACKAGES += \
 # PocketMode
 PRODUCT_PACKAGES += \
     MotoPocketMode
-    
+
+# Perf
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/perf/commonresourceconfigs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/commonresourceconfigs.xml \
+    $(LOCAL_PATH)/configs/perf/perfboostsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostsconfig.xml \
+    $(LOCAL_PATH)/configs/perf/perfconfigstore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfconfigstore.xml \
+    $(LOCAL_PATH)/configs/perf/targetconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/targetconfig.xml \
+    $(LOCAL_PATH)/configs/perf/targetresourceconfigs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/targetresourceconfigs.xml \
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2.vendor \
