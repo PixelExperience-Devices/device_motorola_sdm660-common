76dd672630fbf9ddadf78136c6bad9d26de781c6
jro1979oliver <jroliveira.oliveira301@gmail.com>
2023-06-21 13:14:13 +0200

    sdm660-common: Use Product to call sepolicy instead of system_ext
    Change-Id: Icf01b31fdc0fd9d6480b748ad266aaddd81ebad7

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -134,7 +134,7 @@ VENDOR_SECURITY_PATCH := 2021-02-01
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy/vendor
-SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy/private
+PRODUCT_PRIVATE_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy/private
 PRODUCT_PUBLIC_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy/public
 
 # Treble
