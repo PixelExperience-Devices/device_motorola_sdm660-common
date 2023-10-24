18337e20c4df26e1e755587f31f2061d47103571
Bruno Martins <bgcngm@gmail.com>
2023-10-24 12:07:53 +0100

    sdm660-common: Switch to libprotobuf-cpp prebuilts from v29 VNDK
    Android moved to a versioned approach in R and vendorcompat libs were
    still possible to build, but not anymore with the Android U release.
    
    Change-Id: Ibe06bf8d22ab0ce24ac5d5d9f3394b8593ee46cb

--- common.mk
+++ common.mk
@@ -330,11 +330,6 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
     vendor.qti.hardware.perf@2.0.vendor
 
-# Protobuf
-PRODUCT_PACKAGES += \
-    libprotobuf-cpp-full-vendorcompat \
-    libprotobuf-cpp-lite-vendorcompat
-
 # QMI
 PRODUCT_PACKAGES += \
     libjson
@@ -413,6 +408,11 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-service
 
 # VNDK
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm64/arch-arm-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-lite.so \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full.so \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite.so
+
 # Update this list with what each blob is actually for
 # libstdc++: camera.sdm660
 PRODUCT_PACKAGES += \
