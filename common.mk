15bc281ab7d8b2f8541da577a5f6dbb3f54b0302
Nolen Johnson <johnsonnolen@gmail.com>
2023-11-27 19:33:18 -0500

    sdm660-common: Organize and relabel speed-profiles
    Change-Id: I04ee70894930aef1105840d90da57454352ad009

--- common.mk
+++ common.mk
@@ -380,10 +380,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
-# Speed profile services and wifi-service to reduce RAM and storage
+# Speed Profiles
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
-PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
 
 # Telephony
 PRODUCT_PACKAGES += \
